094519b39e0b9a37c229b720e2f3596c2b6872b5
Eliot Jordan <eliotj@princeton.edu>
2016-09-29 17:40:52 -0500

    use workshop branch

--- install_scripts/demo-app.sh
+++ install_scripts/demo-app.sh
@@ -4,8 +4,9 @@ mkdir demos
 cd demos
 rails new geo-concerns-demo --skip-spring
 cd geo-concerns-demo
-echo "gem 'geo_concerns', '0.0.10'" >> Gemfile
+echo "gem 'geo_concerns', git: 'https://github.com/projecthydra-labs/geo_concerns.git', branch: 'workshop'" >> Gemfile
 bundle install
+sudo bundle install
 rails generate curation_concerns:install -f
 rails generate geo_concerns:install -f
 rake db:migrate
